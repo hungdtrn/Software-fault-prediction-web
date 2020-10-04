8947baa4a1c652dbd64c3eb873f9621a74a1d4e8
hung <hung.dtrn@gmail.com>
2020-10-05 00:43:07 +1100

    docker

--- app/new_client/build/service-worker.js
+++ app/new_client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3d744f156dc066b4b1533c74880fe6a2.js"
+  "./precache-manifest.e6cd19694359095ff03e216c4ecf05c9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
